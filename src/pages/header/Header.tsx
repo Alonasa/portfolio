c4df383bd605ca84c396bddb74bbcb0190ddfcb1
Alonesa <alenakrasnov@mail.ru>
2023-02-18 21:00:21 +0000

    added simple menu

--- src/pages/header/Header.tsx
+++ src/pages/header/Header.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import styles from './Header.module.css'
 
 export const Header = () => {
   return (
-	<nav>
-	  <ul>
-	  <li>Skills</li>
-	  <li>Portfolio</li>
+	<nav className={styles.menu}>
+	  <ul className={styles.menuList}>
+	  <li><a className={styles.menuListLink} href={'#'}> Skills</a></li>
+	  <li><a className={styles.menuListLink} href={'#'}> Portfolio</a></li>
 	  </ul>
-	  <div>
-		<h1><a>Alona Skrypnyk</a></h1>
+	  <div className={styles.menuTitle}>
+		<h1><a className={styles.menuListLink}>Alona Skrypnyk</a></h1>
 	  </div>
-	  <ul>
-		<li>Contacts</li>
-		<li>Make an offer</li>
+	  <ul className={styles.menuList}>
+		<li><a className={styles.menuListLink} href={'#'}> Contacts</a></li>
+		<li><a className={styles.menuListLink} href={'#'}> Make an offer</a></li>
 	  </ul>
 	</nav>
   );
