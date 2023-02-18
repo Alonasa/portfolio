0a7c087c9865bd22712aa82280751da7981008df
Alonesa <alenakrasnov@mail.ru>
2023-02-18 23:46:10 +0000

    change resolution

--- src/pages/header/Header.tsx
+++ src/pages/header/Header.tsx
@@ -8,10 +8,10 @@ export const Header = () => {
 	  <li><NavLink to={'skills'} className={({isActive})=>(isActive? styles.menuListLinkActive : styles.menuListLink)}> Skills</NavLink></li>
 	  <li><NavLink to={'portfolio'} className={({isActive})=>(isActive? styles.menuListLinkActive : styles.menuListLink)}> Portfolio</NavLink></li>
 	  </ul>
-	  <div className={styles.menuTitle}>
-		<h1><NavLink to={''} className={({isActive})=>(isActive? styles.menuListLinkActive : styles.menuListLink)}>Alona Skrypnyk</NavLink></h1>
+	  <div className={styles.menuLogo}>
+		<h1 className={styles.menuTitle}><NavLink to={''} className={({isActive})=>(isActive? styles.menuListLinkActive : styles.menuListLink)}>Alona Skrypnyk</NavLink></h1>
 	  </div>
-	  <ul className={styles.menuList}>
+	  <ul className={`${styles.menuList} ${styles.menuRight}`}>
 		<li><NavLink to={'contacts'} className={({isActive})=>(isActive? styles.menuListLinkActive : styles.menuListLink)}> Contacts</NavLink></li>
 		<li><NavLink to={'make an offer'} className={({isActive})=>(isActive? styles.menuListLinkActive : styles.menuListLink)}> Make an offer</NavLink></li>
 	  </ul>
