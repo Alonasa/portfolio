735a4c50503cbe54633efef6a27863c6440e4eb0
Alonesa <alenakrasnov@mail.ru>
2023-03-05 12:11:01 +0000

    added big components blocks

---
+++ src/pages/main/Hire/Hire.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Hire = () => {
+  return (
+	<div>
+	  Hire block
+	</div>
+  );
+};
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {About} from './About/About';
+import { Contacts } from './Contacts/Contacts';
+import { Hire } from './Hire/Hire';
+import { Portfolio } from './Portfolio/Portfolio';
+import { Skills } from './Skills/Skills';
 
 export const Main = () => {
   return (
-	<div>
-	  Main Content jf
-	</div>
+    <div>
+      <About/>
+      <Skills/>
+      <Portfolio/>
+      <Hire/>
+      <Contacts/>
+    </div>
   );
 };
