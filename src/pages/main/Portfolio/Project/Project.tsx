c07aeb272766335cbc9742d8272e2a222af2adac
Alonesa <alenakrasnov@mail.ru>
2023-03-17 20:32:59 +0000

    added styles for project when it's have big description

--- src/pages/main/Portfolio/Project/Project.tsx
+++ src/pages/main/Portfolio/Project/Project.tsx
@@ -11,8 +11,8 @@ export const Project = () => {
 	  <div className={styles.projectInner}>
 		<h3 className={styles.projectTitle}>Todolist</h3>
 		<p className={styles.projectDescription}>Todolist is a simple project helps you to mannage your schedle and being more productive</p>
-	  	<div>
-		  Technologies: <a href={''}>HTML</a>, <a href={''}>CSS</a>, <a href={''}>React</a>, <a href={''}>Typescript</a>, <a href={''}>MUI</a>
+	  	<div className={styles.projectTechnologies}>
+		  <b>Technologies:</b> <a href={''}>HTML</a>, <a href={''}>CSS</a>, <a href={''}>React</a>, <a href={''}>Typescript</a>, <a href={''}>MUI</a>
 		</div>
 	  </div>
 	</li>
